3701c28933212a7db3cb3d1bf51210a6391987af
Mark Stenglein <mark@stengle.in>
2020-12-13 00:08:16 -0500

    ZSH: Add zsh configuration
    Signed-off-by: Mark Stenglein <mark@stengle.in>

--- pre-install.sh
+++
@@ -1,8 +0,0 @@
-
-# Check if oh-my-zsh is installed
-OMZDIR="$HOME/.oh-my-zsh"
-if [ ! -d "$OMZDIR" ]; then
-  echo 'Installing oh-my-zsh'
-  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
